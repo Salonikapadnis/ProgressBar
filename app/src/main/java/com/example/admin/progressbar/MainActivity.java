86158ddb6971cc8387f4fa80074945329c16a875
ADMIN <sallukapadnis9@gmail.com>
2019-05-31 12:20:46 +0530

    progress bar demo

---
+++ app/src/main/java/com/example/admin/progressbar/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.admin.progressbar;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class  MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private int progressStatus = 0;
+    private TextView textView;
+    private Handler handler = new Handler(); //handler is class which handles the threads.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar3);
+        textView = (TextView) findViewById(R.id.textView2);
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus += 1;
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                            textView.setText(progressStatus + "/" + progressBar.getMax());
+                        }
+                    });
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }}
